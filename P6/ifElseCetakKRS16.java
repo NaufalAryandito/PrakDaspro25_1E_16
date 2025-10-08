ddd0794a61b8c2ec04c50692523bb29485ae7284
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-08 16:19:09 +0700

    P6_Percobaan 2_PertanyaanNo.3

---
+++ P6/ifElseCetakKRS16.java
@@ -0,0 +1,33 @@
+package P6;
+import java.util.Scanner;
+public class ifElseCetakKRS16 {
+    public static void main(String[] args) {
+        Scanner dito = new Scanner(System.in);
+       
+
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Masukkan semester saat ini");
+        int semester16 = dito.nextInt();
+
+        if (semester16 == 1) {
+            System.out.println("KRS semester 1 ditampilkan");
+        }else if (semester16 == 2){
+            System.out.println("KRS semester 2 ditampilkan");
+        }else if(semester16 == 3){
+            System.out.println("KRS semester 3 ditampilkan");
+        }else if (semester16 == 4){
+            System.out.println("KRS semester 4 ditampilkan");
+        }else if (semester16 == 5){
+            System.out.println("KRS semester 5 ditampilkan");
+        }else if (semester16 == 6){
+            System.out.println("KRS semester 6 ditampilkan");
+        }else if (semester16 == 7){
+            System.out.println("KRS semester 7 ditampilkan");
+        } else if (semester16== 8){
+            System.out.println("KRS semester 8 ditampilkan");
+        }else
+        System.out.println("Tidak dapat ditampilkan");
+        }
+        }
+    
+
