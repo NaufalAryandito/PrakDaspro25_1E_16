404a4e3ef77b0cb6dd631d1ca21b37ccdc879a2b
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-08 11:07:42 +0700

    Percobaan 1

---
+++ jobsheet5/ifCetakKRS16.java
@@ -0,0 +1,19 @@
+package jobsheet5;
+import java.util.Scanner;
+
+public class ifCetakKRS16 {
+    public static void main(String[] args) {
+        Scanner dito = new Scanner(System.in);
+        
+        System.out.println("---Cetak KRS SIAKAD---");
+        System.out.println("Apakah UKT sudah lunas? (true/false) : ");
+        boolean uktlunas16= dito.nextBoolean();
+
+        if (uktlunas16){
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+
+        }
+    }
+    
+}
