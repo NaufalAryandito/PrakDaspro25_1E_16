ae8838101676132ab596c149bcec9422992b7936
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-29 12:05:25 +0700

    p190_Percobaan 2 Pertanyaan 1

--- P10/Square16.java
+++ P10/Square16.java
@@ -1,18 +1,19 @@
 package P10;
+
 import java.util.Scanner;
+
 public class Square16 {
     public static void main(String[] args) {
-        Scanner dito= new Scanner(System.in);
+        Scanner dito = new Scanner(System.in);
 
         System.out.print("Masukkan nilai N= ");
-        int N16= dito.nextInt();
-        for (int iouter16=1; iouter16<=N16; iouter16++){
-        for (int i16=1; i16<=N16; i16++){
-            System.out.print("*");
-            
-          
+        int N16 = dito.nextInt();
+        for (int iouter16 = 0; iouter16 <= N16; iouter16++) {
+            for (int i16 = 1; i16 <= N16; i16++) {
+                System.out.print("*");
+
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
 }
