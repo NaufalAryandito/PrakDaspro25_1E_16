21fe7a0b3b6a6d0e64d717f0fa4dad199c16aee0
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-29 11:59:06 +0700

    P10_Percobaan 2

---
+++ P10/Square16.java
@@ -0,0 +1,18 @@
+package P10;
+import java.util.Scanner;
+public class Square16 {
+    public static void main(String[] args) {
+        Scanner dito= new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N= ");
+        int N16= dito.nextInt();
+        for (int iouter16=1; iouter16<=N16; iouter16++){
+        for (int i16=1; i16<=N16; i16++){
+            System.out.print("*");
+            
+          
+        }
+        System.out.println();
+    }
+}
+}
