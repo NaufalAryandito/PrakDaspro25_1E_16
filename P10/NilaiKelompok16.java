7e2c75dee36015a3347c3d3185d2a8e5e126a478
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-31 21:39:26 +0700

    P10_Percobaan 3

---
+++ P10/NilaiKelompok16.java
@@ -0,0 +1,9 @@
+package P10;
+import java.util.Scanner;
+public class NilaiKelompok16 {
+    public static void main(String[] args) {
+        Scanner dito = new Scanner(System.in);
+
+        
+    }
+}
