34f992a76b61c3bc5bc79d1c496ef1368a0e5e07
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-10-31 22:46:31 +0700

    P10_Percobaan 4

--- P10/NilaiKelompok16.java
+++ P10/NilaiKelompok16.java
@@ -3,7 +3,24 @@ import java.util.Scanner;
 public class NilaiKelompok16 {
     public static void main(String[] args) {
         Scanner dito = new Scanner(System.in);
+        int i16= 1, j16;
+        float totalNilai16, nilai16, rataNilai16;
+        while (i16<=6) {
+            System.out.println("Kelompok"+" "+ i16);
 
-        
+            totalNilai16=0;
+            for (j16=1; j16<=5; j16++){
+                System.out.print("\r Nilai Dari Kelompok Penilai"+" " +j16+":");
+                nilai16= dito.nextFloat();
+
+                totalNilai16+=nilai16;
+            }
+            rataNilai16= totalNilai16/5;
+            System.out.println("Kelompok"+" "+i16+":"+ "Nilai Rata Rata="+" "+rataNilai16);
+            i16++;
+            
+        }
+
+
     }
 }
