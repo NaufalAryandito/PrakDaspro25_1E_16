9e23b5bee992752939d2854c9ebe63232d21045a
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-05 14:27:40 +0700

    P11_Percobaan 1 Pertanyaan 2

--- P11/ArrayBilangan16.java
+++ P11/ArrayBilangan16.java
@@ -3,15 +3,25 @@ package P11;
 public class ArrayBilangan16 {
     public static void main(String[] args) {
         
-        int [] bil = new int [4];
-        bil [0]= 5;
-        bil [1]= 13;
-        bil [2]= -7;
-        bil [3]= 17;
+        int[] bil16= {5,13,-7,17};
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+        System.out.println(bil16[0]);
+        System.out.println(bil16[1]);
+        System.out.println(bil16[2]);
+        System.out.println(bil16[3]);
+       
+        
+        
+        // int [] bil = new int [4];
+        // bil [0]= 5;
+        // bil [1]= 13;
+        // bil [2]= -7;
+        // bil [3]= 17;
+        
+        
+        
+        
+        
+        
     }
 }
