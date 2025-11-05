f9f3f3d6ea9e264f57d7a8019bd1977e66d2d7ad
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-05 20:29:26 +0700

    P11_Percobaan 3_Pertanyaan 1

--- P11/Arrayratanilai16.java
+++ P11/Arrayratanilai16.java
@@ -8,14 +8,19 @@ public class Arrayratanilai16 {
         int[] nilaiMhs16= new int[10];
         double total16=0;
         double rata216;
+        int lulus16=0;
 
         for (int i=0; i< nilaiMhs16.length; i++){
             System.out.print("Masukkan nilai mahasiswa ke- "+(i+1)+" : ");
             nilaiMhs16[i]= dito.nextInt();
 
-            total16+=nilaiMhs16[i];
+            if(nilaiMhs16[i]>70){
+                lulus16++;
+            }
+                total16+=nilaiMhs16[i];
         }
         rata216= total16/nilaiMhs16.length;
         System.out.println("Rata-Rata nilai= "+rata216);
+        System.out.println("Lulus: "+ lulus16+" Siswa");
     }
 }
