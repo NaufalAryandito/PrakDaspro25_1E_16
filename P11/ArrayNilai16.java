a0cebf8b46cc575eb9e81aee01d4a153d9256faf
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-05 19:27:51 +0700

    P10_Percobaan 2_pertanyaan 3

--- P11/ArrayNilai16.java
+++ P11/ArrayNilai16.java
@@ -11,6 +11,9 @@ public class ArrayNilai16 {
         System.out.print("Masukkan nilai akhir ke- "+i+" : ");
         nilaiAkhir16[i]= dito.nextInt();
         
+        if (nilaiAkhir16[i]>70){
+            System.out.println("Mahasiswa ke- "+ i +" Lulus!");
+        }
     }
     
     for (int i=0; i<10; i++){
