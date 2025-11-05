1d733f3a524bf70a508f009a1218ac88ce9bca3e
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-05 14:41:33 +0700

    P11_Percobaan 1_ Pertanyaan 4

--- P11/ArrayBilangan16.java
+++ P11/ArrayBilangan16.java
@@ -5,7 +5,7 @@ public class ArrayBilangan16 {
         
         int[] bil16= {5,13,-7,17};
 
-        for (int i=0; i < 4; i++){
+        for (int i=0; i <= 4; i++){
             System.out.println(bil16[i]);
         }
         
