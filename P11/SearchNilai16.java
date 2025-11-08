596a96e9d8f2765b62d397daadb72bef30bab40d
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-08 10:07:49 +0700

    P11_Percobaan 4_Pertanyaan 2

---
+++ P11/SearchNilai16.java
@@ -0,0 +1,41 @@
+package P11;
+import java.util.Scanner;
+public class SearchNilai16 {
+    public static void main(String[] args) {
+        Scanner dito = new Scanner(System.in);
+        int key16;
+        int hasil16=0;
+        
+        System.out.print("Masukkan nilai yang akan diinput= ");
+        int jml16= dito.nextInt();
+        
+        int []  arrNilai16= new int[jml16];
+        
+        for (int i=0; i<jml16;i++){
+            System.out.println("Masukkan nilai mahasiswa ke- "+ (i+1) + " = ");
+            arrNilai16[i]= dito.nextInt();
+        }
+        
+        
+        System.out.println("Masukkan nilai yang akan dicari= ");
+        key16= dito.nextInt();
+        
+        for (int i=0; i<arrNilai16.length; i++){
+            if (arrNilai16[i] == key16){
+                hasil16 = i+1;
+            
+            }
+            // if (key16!=i){
+
+            //     System.out.println("");
+            // }
+        }
+
+       
+       
+       
+        System.out.println();
+        System.out.println("Nilai "+ key16+" Ketemu, merupakan nilai mahasiswa ke- "+hasil16);
+        System.out.println();
+    }
+}
