c21cfa0c1cdb255027b9b25a08128f66cc0f60fa
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-12 11:15:47 +0700

    P12_Percobaan 1

---
+++ P12/Bioskop16.java
@@ -0,0 +1,20 @@
+package P12;
+
+public class Bioskop16 {
+    public static void main(String[] args) {
+        
+        String [][] penonton16= new String[4][2];
+        penonton16[0][0]= "Amin";
+         penonton16[0][1]="Bena";
+         penonton16[1][0]="Candra";
+         penonton16[1][1]="Dela";
+         penonton16[2][0]="Eka";
+         penonton16[2][1]="Farhan";
+         penonton16[3][0]="Gisel";
+
+         System.out.printf("%s \t %s\n", penonton16[0][0], penonton16[0][1]);
+         System.out.printf("%s \t %s\n", penonton16[1][0], penonton16[1][1]);
+          System.out.printf("%s \t %s\n", penonton16[2][0], penonton16[2][1]);
+           System.out.printf("%s \t %s\n", penonton16[3][0], penonton16[3][1]);
+        }
+}
