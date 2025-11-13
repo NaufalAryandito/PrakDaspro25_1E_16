34caf652e95d56a6168424179290c7d5a28f721c
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-13 10:42:15 +0700

    P12_Percobaan 3_Pertanyaan 4

--- P12/Numbers16.java
+++ P12/Numbers16.java
@@ -12,6 +12,7 @@ public class Numbers16 {
 
         for (int i = 0; i < myNumbers16.length; i++) {
             System.out.println(Arrays.toString(myNumbers16[i]));
+            System.out.println("Panjang baris ke- "+(i+1)+" : "+ myNumbers16[i].length);
         }
     }
 }
