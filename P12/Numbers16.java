648fc86bb745ad8da50e0e11645d8f67a5456b5c
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-13 10:23:13 +0700

    P12_Percobaan 3

---
+++ P12/Numbers16.java
@@ -0,0 +1,12 @@
+package P12;
+
+public class Numbers16 {
+    public static void main(String[] args) {
+        int[][] myNumbers16= new int[3][];
+        
+        myNumbers16[0]= new int[5];
+        myNumbers16[1]= new int[3];
+        myNumbers16[2]= new int[1];
+
+    }
+}
