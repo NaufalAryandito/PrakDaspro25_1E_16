b4ce82a46639d81f8dfbf5a5799705262a00ba5d
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-26 15:11:36 +0700

    P14_ Percobaan 1_ Pertanyaan 2

--- P14/Kafe16.java
+++ P14/Kafe16.java
@@ -10,11 +10,11 @@ public class Kafe16 {
         System.out.println("4. Teh Tarik -Rp 12,000");
         System.out.println("5. Roti Bakar -Rp 10,000");
         System.out.println("6. Mie Goreng -Rp 18,000");
-        System.out.println("===========================");
+        System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
     }
 
     public static void main(String[] args) {
-        Menu16();
+        // Menu16();
     }
 }
