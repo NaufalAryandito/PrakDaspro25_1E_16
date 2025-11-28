230b44f780b2393c1c2206263eb27ed1f6726212
NaufalAryandito <muhammadnaufalaryanditoabrar@gmail.com>
2025-11-28 20:30:39 +0700

    P14_Percobaan2_Pertanyaan6

--- P14/Kafe2_16.java
+++ P14/Kafe2_16.java
@@ -1,7 +1,7 @@
 package P14;
 public class Kafe2_16 {
 
-    public static void Menu16(String namapelanggan16, boolean isMember) {
+    public static void Menu16(String namapelanggan16, boolean isMember, String kodePromo16) {
         System.out.println("Selamat datang, "+namapelanggan16+" !");
         if (isMember) {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian");
@@ -16,8 +16,19 @@ public class Kafe2_16 {
         System.out.println("6. Mie Goreng -Rp 18,000");
         System.out.println("==========================");
         System.out.println("Silahkan pilih menu yang anda inginkan");
+
+        if (kodePromo16.equals("DISKON50")) {
+            System.out.println("Kode promo valid: Diskon 50%");
+        }else if (kodePromo16.equals("DISKON30")){
+            System.out.println("Kode promo valid: Diskon 30%");
+        }else {
+            System.out.println("Kode promo tidak valid");
+        }
+
     }
     public static void main(String[] args) {
-        Menu16("Andi", true);
+        Menu16("Andi", true, "DISKON50");
+        Menu16("Dito", true, "DISKON30");
+        Menu16("Arjun", true, "DISKON20");
     }
 }
